6f393dfbfbcd77d2421849e73cda03427cc01372
freeze1 <humera.rk@gmail.com>
2021-10-11 07:28:58 -0700

    trying to incorporate pull requests

--- Flask-Web-App-Tutorial/website/auth.py
+++ Flask-Web-App-Tutorial/website/auth.py
@@ -3,7 +3,7 @@ from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from flask_login import login_user, login_required, logout_user, current_user
-
+# This is used for creating authorization
 
 auth = Blueprint('auth', __name__)
 
